b29d882398ef8430611a1cb31ac2cc87bed9a8b7
Devin Lim <davay@devinl.im>
2024-07-28 22:29:35 -0700

    remove yadm, just use ansible

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-A YADM + Ansible setup, run ./init.sh to start
+```
+sudo dnf update -y
+sudo dnf install -y git vim epel-release
+git clone https://github.com/davay/hetzner-setup.git ~/repos/
+sudo dnf install ansible -y
+ansible-playbook ~/repos/playbooks/heihachi.yml
+```
--- init.sh
+++
@@ -1,7 +0,0 @@
-# install yadm 
-sudo dnf update -y
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:TheLocehiliosan:yadm/Fedora_40/home:TheLocehiliosan:yadm.repo
-sudo dnf install yadm
-
-# use yadm to kickstart installation
-yadm clone https://github.com/davay/hetzner-setup.git
