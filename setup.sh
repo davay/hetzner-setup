5e27f8225614cfec54cf0c61e003e8ae5facd3b0
Devin Lim <davay@devinl.im>
2025-03-27 09:52:24 -0700

    minor: readme

--- setup.sh
+++ setup.sh
@@ -7,11 +7,9 @@ read -rsp "AWS_SECRET_KEY: " AWS_SECRET_KEY
 echo
 
 sudo dnf install -y ansible
-git clone https://github.com/davay/hetzner-setup.git ~/Repos/hetzner-setup
 
 # SUDO IS PASSWORDLESS HERE
 ansible-playbook ./playbooks/init.yml
 ansible-playbook ./playbooks/firewalld.yml
 ansible-playbook ./playbooks/nginx.yml -e "aws_access_key=$AWS_ACCESS_KEY aws_secret_key=$AWS_SECRET_KEY"
 # ansible-playbook ./playbooks/heihachi.yml
-
